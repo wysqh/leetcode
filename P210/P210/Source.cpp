49235f7d2041911f40faa5f6aac0bdef77891bcb
wysqh <141110326@stu.hit.edu.cn>
2017-09-13 20:58:32 +0800

    A quicker version of P210

--- P210/P210/Source.cpp
+++ P210/P210/Source.cpp
@@ -1,7 +1,60 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 #include <algorithm>
 using namespace std;
+
+/*
+ A Quicker Version
+*/
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<pair<int, int>>& prerequisites) {
+        unordered_map<int, vector<int>> sides;
+        unordered_map<int, list<int>> degrees;
+        unordered_map<int, list<int>::iterator> iters;
+        
+        vector<int> indegrees = vector<int>(numCourses + 1, 0);
+        
+        for(int i = 0; i < prerequisites.size(); i++)
+        {
+            sides[prerequisites[i].second].push_back(prerequisites[i].first);
+            indegrees[prerequisites[i].first]++;
+        }
+        
+        for(int i = 0; i < numCourses; i++)
+        {
+            degrees[indegrees[i]].push_back(i);
+            iters[i] = (--degrees[indegrees[i]].end());
+        }
+        
+        vector<int> answer;
+        
+        while(!degrees[0].empty())
+        {
+            int k = degrees[0].back();
+            degrees[0].pop_back();
+            answer.push_back(k);
+            
+            for(int i = 0; i < sides[k].size(); i++)
+            {
+                int lastin = indegrees[sides[k][i]];
+                indegrees[sides[k][i]]--;
+                
+                degrees[lastin].erase(iters[sides[k][i]]);
+                degrees[lastin - 1].push_back(sides[k][i]);
+                iters[sides[k][i]] = (--degrees[lastin - 1].end());
+            }
+        }
+        
+        if(answer.size() < numCourses)
+        {
+            answer.clear();
+        }
+        
+        return answer;
+    }
+};
 
 class Solution {
 public:
